e55e616bae335b695c3000e2a18253807e08e5fc
Gabriel Fernandes <gabrielganji01011010@gmail.com>
2025-01-31 13:20:57 -0300

    Adjusting spend's graphic

--- src/components/main/chart/index.tsx
+++ src/components/main/chart/index.tsx
@@ -1,18 +1,60 @@
+import { useState } from "react";
 import Chart from "react-apexcharts";
+import { ApexOptions } from "apexcharts";
 
-const chartConfig = {
-    type: "bar" as "bar",
-    height: '100%',
-    series: [
+export default function ExpensesChart() {
+
+    const [selectedBar, setSelectedBar] = useState<number | null>(3);
+
+    const getMonths = () => {
+
+        const months: Record<string, number> = {
+            "Jan": 1, "Feb": 2, "Mar": 3, "Abr": 4, "May": 5, "Jun": 6, "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12,
+        };
+
+        const actualDate: Date = new Date();
+        const actualMonth: number = actualDate.getMonth() + 1;
+
+        const sixMonths: Array<number> = [actualMonth];
+
+        for (let i = 1; i <= 3; i++) {
+            let prevMonth: number = actualMonth - i;
+            if (prevMonth <= 0) {
+                prevMonth += 12;
+            }
+            sixMonths.unshift(prevMonth);
+        }
+
+        for (let i2 = 1; i2 <= 2; i2++) {
+            let nextMonth: number = actualMonth + i2;
+            if (nextMonth > 12) {
+                nextMonth -= 12;
+            }
+            sixMonths.push(nextMonth);
+        }
+
+        const sixMonthsName = sixMonths.map(value => Object.keys(months).find(key => months[key] === value)).filter(item => item !== undefined);
+
+        return sixMonthsName;
+    }
+
+    const series: { name: string, data: Array<number> }[] = [
         {
             name: "Sales",
-            data: [50, 300, 420, 600, 250, 380,],
+            data: [50, 300, 420, 600, 250, 380],
         },
-    ],
-    options: {
+    ];
+
+    const options: ApexOptions = {
         chart: {
+            type: "bar",
             toolbar: {
                 show: false,
+            },
+            events: {
+                dataPointSelection: (_event, _chartContext, config) => {
+                    setSelectedBar(config.dataPointIndex);
+                },
             },
         },
         stroke: {
@@ -23,11 +65,33 @@ const chartConfig = {
         dataLabels: {
             enabled: false,
         },
-        colors: ["#1b1b1b"],
+        legend: {
+            show: false,
+        },
+        fill: {
+            opacity: 1,
+        },
+        states: {
+            hover: {
+                filter: {
+                    type: "none",
+                },
+            },
+            active: {
+                allowMultipleDataPointsSelection: false,
+                filter: {
+                    type: "none",
+                },
+            },
+        },
+        colors: series[0].data.map((_, index) =>
+            index === selectedBar ? "#66BB6A" : "#1b1b1b"
+        ),
         plotOptions: {
             bar: {
                 columnWidth: "100%",
                 borderRadius: 2,
+                distributed: true,
             },
         },
         xaxis: {
@@ -45,51 +109,19 @@ const chartConfig = {
                     fontWeight: 400,
                 },
             },
-            categories: [
-                "Jan",
-                "Feb",
-                "Mar",
-                "Jun",
-                "Jul",
-                "Aug",
-            ],
+            categories: getMonths(),
         },
         yaxis: {
             show: false,
-            labels: {
-                style: {
-                    colors: "#616161",
-                    fontSize: "12px",
-                    fontFamily: "inherit",
-                    fontWeight: 400,
-                },
-            },
         },
         grid: {
             show: false,
-            borderColor: "#dddddd",
-            strokeDashArray: 5,
-            xaxis: {
-                lines: {
-                    show: true,
-                },
-            },
-            padding: {
-                top: 5,
-                right: 20,
-            },
-        },
-        fill: {
-            opacity: 0.8,
         },
         tooltip: {
             theme: "dark",
+            x: { show: false },
         },
-    },
-};
+    };
 
-export default function ExpensesChart() {
-    return (
-        <Chart {...chartConfig} />
-    );
+    return <Chart options={options} series={series} type="bar" height="100%" />;
 }
